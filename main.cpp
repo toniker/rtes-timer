21c3e795a1e8a7146ba52bd8346490d8cadfa94a
Antonis Keremidis <tkeremidis@gmail.com>
2023-07-10 16:59:43 +0300

    Redo everything

--- main.cpp
+++ main.cpp
@@ -1,88 +1,192 @@
-#include <iostream>
-#include <cmath>
-#include <cstdint>
+#include <queue>
+#include <functional>
+#include <sys/time.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <chrono>
+#include <thread>
+#include <utility>
 
 #include "main.hpp"
 
-void* production(void *arg) {
-    auto userData = *static_cast<double *>(arg);
+struct workFunction {
+    TimerFcn timerFcn;
+    void *userData{};
+};
 
-    auto workArray_ptr = malloc(NumberOfJobs * sizeof(workFunction));
-    if (workArray_ptr == nullptr) {
-        std::cout << "Error initializing workArray" << std::endl;
-        pthread_exit(nullptr);
+class Timer {
+public:
+    Timer(int _period, int _tasksToExecute, int _startDelay, StartFcn _startFcn, StopFcn _stopFcn,
+          TimerFcn _timerFcn, ErrorFcn _errorFcn, void *_userData)
+            : period(_period),
+              tasksToExecute(_tasksToExecute),
+              startDelay(_startDelay),
+              startFcn(std::move(_startFcn)),
+              stopFcn(std::move(_stopFcn)),
+              timerFcn(std::move(_timerFcn)),
+              errorFcn(std::move(_errorFcn)),
+              userData(_userData),
+              running(false),
+              taskCount(0) {
+        if (startDelay > 0)
+            std::this_thread::sleep_for(std::chrono::seconds(startDelay));
     }
-    auto workArray = static_cast<workFunction *>(workArray_ptr);
-    double angle = 30;
-    workArray[0].work = calculate_sin;
-    workArray[1].work = calculate_cos;
-    workArray[2].work = hello;
-    workArray[3].work = tan;
-    workArray[4].work = atan;
 
-    for (uint8_t i = 0; i < NumberOfJobs; i++) {
-        workArray[i].arg = &angle;
-        pthread_mutex_lock(q.mutex);
+    ~Timer() {
+        Stop();
+    }
 
-        addToQueue(&q, workArray[i]);
-        pthread_cond_signal(q.has_items);
-        while (q.head == q.tail) {
-            pthread_cond_wait(q.has_space, q.mutex);
-        }
+    void Start() {
+        if (running)
+            return;
 
-        pthread_mutex_unlock(q.mutex);
-        useconds_t sleepDuration = 1'000'000;
-        gettimeofday(&lastCallTime, nullptr);
-        usleep(sleepDuration);
+        running = true;
+        if (startFcn)
+            startFcn();
+
+        pthread_create(&producerThread, nullptr, &Timer::ProducerThreadFuncHelper, this);
+        pthread_create(&consumerThread, nullptr, &Timer::ConsumerThreadFuncHelper, this);
+    }
+
+    void StartAt(int year, int month, int day, int hour, int minute, int second) {
+        timeval currentTime{};
         gettimeofday(&currentTime, nullptr);
+        long long currentTimestamp = currentTime.tv_sec * 1000LL + currentTime.tv_usec / 1000LL;
+
+        tm targetTime{};
+        targetTime.tm_year = year - 1900;
+        targetTime.tm_mon = month - 1;
+        targetTime.tm_mday = day;
+        targetTime.tm_hour = hour;
+        targetTime.tm_min = minute;
+        targetTime.tm_sec = second;
+
+        time_t targetTimestamp = mktime(&targetTime);
+        long long targetMillis = targetTimestamp * 1000LL;
+
+        long long startDelayMillis = targetMillis - currentTimestamp;
+        if (startDelayMillis > 0)
+            std::this_thread::sleep_for(std::chrono::milliseconds(startDelayMillis));
+
+        Start();
     }
 
-    pthread_exit(nullptr);
-}
+    void Stop() {
+        if (!running)
+            return;
 
-void* consumption(void *arg) {
-    timeval time{};
+        running = false;
+        pthread_join(producerThread, nullptr);
+        pthread_join(consumerThread, nullptr);
 
-    while (true) {
-        pthread_cond_wait(q.has_items, q.mutex);
-        pthread_mutex_lock(q.mutex);
-
-        workFunction workFunction{};
-        pthread_cond_signal(q.has_space);
-
-        workFunction.work(static_cast<double *>(workFunction.arg));
+        if (stopFcn)
+            stopFcn();
     }
 
-    pthread_exit(nullptr);
-}
+private:
+    static void *ProducerThreadFuncHelper(void *arg) {
+        auto timer = static_cast<Timer *>(arg);
+        timer->ProducerThreadFunc();
+        return nullptr;
+    }
 
-void start() {
+    static void *ConsumerThreadFuncHelper(void *arg) {
+        auto timer = static_cast<Timer *>(arg);
+        timer->ConsumerThreadFunc();
+        return nullptr;
+    }
 
-}
+    void ProducerThreadFunc() {
+        while (running) {
+            timeval currentTime{};
+            gettimeofday(&currentTime, nullptr);
+            long long currentTimestamp = currentTime.tv_sec * 1000LL + currentTime.tv_usec / 1000LL;
+
+            if (currentTimestamp >= nextExecutionTime) {
+                if (taskCount < tasksToExecute) {
+                    workFunction task;
+                    task.timerFcn = timerFcn;
+                    task.userData = userData;
+
+                    pthread_mutex_lock(&queueMutex);
+                    taskQueue.push(task);
+                    taskCount++;
+
+                    nextExecutionTime = currentTimestamp + period;
+
+                    pthread_cond_signal(&queueCondition);
+                    pthread_mutex_unlock(&queueMutex);
+                } else {
+                    running = false;
+                }
+            } else {
+                long long timeToSleep = nextExecutionTime - currentTimestamp;
+                std::this_thread::sleep_for(std::chrono::milliseconds(timeToSleep));
+            }
+        }
+    }
+
+    void ConsumerThreadFunc() {
+        while (running) {
+            workFunction task;
+
+            pthread_mutex_lock(&queueMutex);
+            while (taskQueue.empty() && running)
+                pthread_cond_wait(&queueCondition, &queueMutex);
+
+            if (!taskQueue.empty()) {
+                task = taskQueue.front();
+                taskQueue.pop();
+            }
+            pthread_mutex_unlock(&queueMutex);
+
+            if (!task.timerFcn) {
+                std::this_thread::yield();
+                continue;
+            }
+
+            auto data = static_cast<double*>(task.userData);
+            task.timerFcn(data);
+        }
+    }
+
+    int period;
+    int tasksToExecute;
+    int startDelay;
+    StartFcn startFcn;
+    StopFcn stopFcn;
+    TimerFcn timerFcn;
+    ErrorFcn errorFcn;
+    void *userData;
+
+    pthread_t producerThread;
+    pthread_t consumerThread;
+    std::queue<workFunction> taskQueue;
+    pthread_mutex_t queueMutex;
+    pthread_cond_t queueCondition;
+
+    bool running;
+    int taskCount;
+    long long nextExecutionTime;
+};
 
 int main() {
-    UserData = new(double);
-    *UserData = 5.134;
+    int period = 50; // milliseconds
+    int tasksToExecute = 5;
+    int startDelay = 1; // seconds
+    StartFcn startFcn = MyStartFcn;
+    StopFcn stopFcn = MyStopFcn;
+    TimerFcn timerFcn = MyTimerFcn;
+    auto userData = new(double);
+    *userData = 3.14;
+    ErrorFcn errorFcn = MyErrorFcn;
 
-    Queue queue;
-    struct data {
-        Queue* q;
-        double* d;
-    };
-    data data = {&queue, UserData};
+    Timer timer(period, tasksToExecute, startDelay, startFcn, stopFcn, timerFcn, errorFcn, userData);
+    timer.Start();
 
-    StartFcn();
-    pthread_t producer, consumer;
+    // Start the timer at a specific time
+//    timer.StartAt(2023, 7, 10, 16, 37, 0);
 
-    pthread_create(&producer, nullptr, &production, &data);
-    pthread_create(&consumer, nullptr, &consumption, &data);
+    // Wait for the timer to finish
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 
-    pthread_join(producer, nullptr);
-    pthread_join(consumer, nullptr);
-
-    StopFcn();
     return 0;
 }
--- main.hpp
+++ main.hpp
@@ -1,95 +1,25 @@
 #pragma once
-#include "etl/queue_mpmc_mutex.h"
 
-struct workFunction {
-    void (*work)(double *);
+#include <iostream>
 
-    void *arg;
-};
+typedef std::function<void(double* arg)> TimerFcn;
+typedef std::function<void()> StartFcn;
+typedef std::function<void()> StopFcn;
+typedef std::function<void()> ErrorFcn;
 
-inline constexpr uint8_t QueueSize = 10;
-inline constexpr uint8_t NumberOfJobs = 5;
-
-inline void initialize()
-{
-    std::cout << "Starting" << std::endl;
+void MyTimerFcn(const double* arg) {
+    auto data = *arg;
+    std::cout << "Received data = " << data << std::endl;
 }
 
-inline void periodic() {
-    std::cout << "Periodic" << std::endl;
+void MyStartFcn() {
+    std::cout << "Timer started." << std::endl;
 }
 
-inline void reportQueueFullError() {
-    std::cout << "Queue Full!" << std::endl;
+void MyStopFcn() {
+    std::cout << "Timer stopped." << std::endl;
 }
 
-inline void stop()
-{
-    std::cout << "Stopping" << std::endl;
+void MyErrorFcn() {
+    std::cout << "Queue is full." << std::endl;
 }
-
-/**
- * The delay between function calls, in milliseconds.
-*/
-inline constexpr uint16_t Period = 20;
-/**
- * How many times the TimerFcn will be executed.
-*/
-inline constexpr uint8_t TasksToExecute = 5;
-/**
- * Delay before the first call to TimerFcn.
-*/
-inline constexpr uint16_t StartDelay = 4;
-/**
- * The function that will be called during the Timer constructor for data initialization
-*/
-inline constexpr void (*StartFcn)() = {initialize};
-/**
- * The function that will be called after the last TimerFcn call.
-*/
-inline constexpr void (*StopFcn)() = {stop};
-/**
- * The function that will be called periodically.
-*/
-inline constexpr void (*TimerFcn)() = {periodic};
-/**
- * The function that will be called if the task queue is full.
-*/
-inline constexpr void (*ErrorFcn)() = {reportQueueFullError};
-/**
- * Pointer to user data
-*/
-inline double* UserData;
-
-
-void calculate_sin(double *arg) {
-    double angle = *arg;
-    *arg = sin(angle);
-}
-
-void calculate_cos(double *arg) {
-    double angle = *arg;
-    *arg = cos(angle);
-}
-
-void hello(double *arg) {
-    double value = *arg;
-    std::cout << "Hello from the thread! value: " << value << std::endl;
-}
-
-void tan(double *arg) {
-    double angle = *arg;
-    *arg = tan(angle);
-}
-
-void atan(double *arg) {
-    double angle = *arg;
-    *arg = atan(angle);
-}
-
-class Queue {
-public:
-    etl::queue_mpmc_mutex<workFunction, QueueSize> queue;
-
-
-};
